fde7b92ffae811b69e4ec46d06d63cc5591453dc
khalid hassan <abuuanas30@hotmail.com>
2018-07-01 12:37:31 +0300

    debuging idb

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,30 @@
-import Database from './utils/indexDb';
+import Database from './utils/index';
 
 document.addEventListener('DOMContentLoaded', () => {
     const endpoint = 'https://free.currencyconverterapi.com/api/v5/currencies';
     const inputCurrency = document.querySelector('.currency_convert_from');
     const outputCurrency = document.querySelector('.currency_convert_to');
     const convertButton = document.querySelector('#convert_currency');
-    const currencyTobeConverted = document.querySelector('input#currency_to_be_converted',);
+    const originalCurrencyInput = document.querySelector('input#original_currency',);
     const convertedCurrencyOutput = document.querySelector('input#converted_currency',);
 
 
-
+    // option node for select element
     function createOption(currency) {
         if (currency === "undefined") {
             return 'Currency cannot be empty.';
         }
 
-        let optionNode = document.createElement('option');
+        const optionNode = document.createElement('option');
         optionNode.innerText = currency;
 
         return optionNode;
     }
 
-
+    // add currency nodes to select elements
     function addCurrenciesToSelect(currencies) {
         if (currencies.length === 0 || currencies === 'undefined') {
-            return "Currencies cannot be empty.";
+            console.log("Currencies cannot be empty.");
         }
 
         currencies.map(currency => {
@@ -33,67 +33,87 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-
+    // Get amount in input field
     function getInput() {
         return document.querySelector('input#amount').value;
     }
 
-
+    // get currencies list from api
     function fetchListOfCurrencies() {
         fetch(endpoint)
             .then(response => response.json())
             .then(myJson => {
                 const currencies = Object.keys(myJson.results).sort();
 
+
+                Database.addCurrencyArray('Currencies', 'currenciesArray', currencies);
                 addCurrenciesToSelect(currencies);
+
             })
-            .catch(err =>
-                console.error(
-                    `The following error occured while getting currencies. ${err}`,
-                ),
+            .catch(err => {
+                    console.error(
+                        `The following error occurred while getting currencies. ${err}`,
+                    )
+
+                    // Get rates when user is offline
+                    Database.retrieve('Currencies', 'currenciesArray').then(currencies => {
+                        if (typeof currencies === 'undefined') return;
+                        addCurrenciesToSelect(currencies);
+                    })
+                }
+
             );
     }
 
-
-    function fetchCurrencyRate(url) {
+    // get conversion rates between currencies
+    function fetchCurrencyRate(url, queryString) {
         if (url === 'undefined') {
             return 'URL cannot be empty.';
         }
+        const inputAmount = getInput();
 
         fetch(url)
             .then(response => response.json())
             .then(myJson => {
-                const inputAmount = getInput();
+
                 const exchangeRate = Object.values(myJson);
+
+                Database.addCurrency('ExchangeRates', queryString, exchangeRate);
 
                 calculateExchangeRate(...exchangeRate, inputAmount);
             })
-            .catch(err =>
-                console.log(
-                    `The following error occured while getting the conversion rate. ${err}`,
-                ),
+            .catch(err => {
+                    console.error(
+                        `The following error occured while getting the conversion rate. ${err}`,
+                    );
+
+                    // Get exchange rates when offline
+                    Database.retrieve( 'ExchangeRates' , queryString).then(data => {
+                        if (typeof data === 'undefined') return;
+                        calculateExchangeRate(data, inputAmount);
+                    })
+                }
+
             );
     }
 
+    // api url from which to get conversion rates
+    function buildAPIUrl(queryString) {
 
-    function buildAPIUrl(currency1, currency2) {
-        if (arguments.length !== 2) {
-            return 'A source and destination currencies need to be provided to build the URL.';
-        }
-
-        return `https://free.currencyconverterapi.com/api/v5/convert?q=${currency1}_${currency2}&compact=ultra`;
+        return `https://free.currencyconverterapi.com/api/v5/convert?q=${queryString}&compact=ultra`;
     }
 
-
+    // use selected currencies to get exchange rate
     function getExchangeRate() {
         const sourceCurrency = document.querySelector('.currency_convert_from').value;
         const destinationCurrency = document.querySelector('.currency_convert_to').value;
+        const currencyQueryString = `${sourceCurrency}_${destinationCurrency}`;
 
-        const url = buildAPIUrl(sourceCurrency, destinationCurrency);
-        fetchCurrencyRate(url);
+        const url = buildAPIUrl(currencyQueryString);
+        fetchCurrencyRate(url, currencyQueryString);
     }
 
-
+    // calculate exchange rates using amount entered
     function calculateExchangeRate(exchangeRate, input) {
         if (arguments.length !== 2) {
             return 'An input amount and exchange rate must be provided.';
@@ -101,16 +121,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const convertedCurrency = input * exchangeRate;
 
-        currencyTobeConverted.value = input;
+        originalCurrencyInput.value = input;
         convertedCurrencyOutput.value = convertedCurrency.toFixed(2);
     }
 
-
+    // add event listener to the convertButton
     function addEventListeners() {
         convertButton.addEventListener('click', getExchangeRate);
     }
 
-
+    // add functions to be called to main
     function main() {
         addEventListeners();
         fetchListOfCurrencies();
---
+++ src/js/utils/index.js
@@ -0,0 +1,79 @@
+import idb from 'idb';
+
+const dbPromise = idb.open('converter', 1, upgradeDb => {
+            const CurrenciesStore = upgradeDb.createObjectStore('Currencies');
+            const ExchangeRate = upgradeDb.createObjectStore('ExchangeRates');
+    }
+);
+
+
+export default class Database {
+    static addCurrencyArray (dbStore, key, data) {
+        return dbPromise.then( db => {
+            const tx = db.transaction(dbStore, 'readwrite');
+            const store = tx.objectStore(dbStore);
+            store.put(data);
+            return tx.complete;
+        });
+    };
+
+    static addCurrency(dbStore, key, data) {
+        return dbPromise.then( db => {
+            const tx = db.transaction(dbStore, 'readwrite');
+            const store = tx.objectStore(dbStore);
+            data.forEach(currency => store.put(currency));
+            return tx.complete;
+        });
+    };
+
+    static retrieve(dbStore, key) {
+        return dbPromise.then( db => {
+            const tx = db.transaction(dbStore);
+            const store = tx.objectStore(dbStore);
+
+            return store.get(key);
+        });
+    };
+
+    static search (dbStore, dbIndex, searchKey, searchValue) {
+        let results = [];
+        return dbPromise.then(db => {
+            const tx = db.transaction(dbStore, 'readwrite');
+            const store = tx.objectStore(dbStore);
+
+            if ( !dbIndex ) { return store.openCursor(); }
+            const index = store.index(dbIndex);
+            return index.openCursor();
+        })
+            .then(function findItem(cursor) {
+                if (!cursor) return;
+                if (cursor.value[searchKey] === searchValue) {
+                    results.push(cursor.value);
+                }
+                return cursor.continue().then(findItem);
+            })
+            .then(function () {
+                return results;
+            })
+    };
+
+    static remove(dbStore, dbIndex, searchKey, searchValue) {
+        return dbPromise.then( db => {
+            const tx = db.transaction(dbStore, 'readwrite');
+            const store = tx.objectStore(dbStore);
+
+            if ( !dbIndex ) { return store.openCursor(); }
+            const index = store.index(dbIndex);
+            return index.openCursor();
+        })
+            .then(function deleteItem(cursor) {
+                if (!cursor) return;
+                if ( cursor.value[searchKey] === searchValue ) {
+                    cursor.delete();
+                }
+                return cursor.continue().then(deleteItem);
+            })
+            .then(function() { return true; })
+    };
+
+}
